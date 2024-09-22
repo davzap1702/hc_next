17de98c31203ee92afcc7dc8fe31af9890e742a5
David Zapata <davidecf@github.com>
2024-09-22 13:32:41 -0500

    feat(vary): - Added a new hook to fetch the three up component data - Added a new interface for the three up component - Added a new breadcrumb component - Added a new preview page for articles - Updated the article page to use the new three up hook

--- hooks/pageHooks/UseHomepageComponents.tsx
+++ hooks/pageHooks/UseHomepageComponents.tsx
@@ -3,9 +3,13 @@ import { INewsComponent } from '@/interfaces/homepgeNews';
 import React from 'react';
 import { useFourUp } from '../useFourUp';
 import { FourUp } from '@/components/blog/FourUp';
+import { useThreeUp } from '../useThreeUp';
+import { ThreeUp } from '@/components/blog/ThreeUp';
+import { IHomeThreeUp } from '@/interfaces/ThreeUp';
 
 type Component =
 | INewsComponent
+| IHomeThreeUp;
 
 interface IHomepageComponentsProps {
     components: Array<Component>
@@ -16,7 +20,9 @@ export const UseHomepageComponents = ({ components }: IHomepageComponentsProps) 
   return components?.map((component, _i) => {
     switch(component.__component){
     case 'shared.news':
-      return <FourUp {...useFourUp(component)} key={_i} />;
+      return <FourUp {...useFourUp(component as INewsComponent)} key={_i} />;
+    case 'shared.three-up':
+      return <ThreeUp {...useThreeUp(component as IHomeThreeUp)} key={_i} />;
     }
   });
 };
--- lib/requests.ts
+++ lib/requests.ts
@@ -1,12 +1,16 @@
 
+import { articleEndpoint, articleEndpointById, articlesByCategory, articlesEndpoints, homepageEndpoint } from './routes';
 import { IArticle, IHomePage } from '@/interfaces';
-import { articleEndpoint, articlesByCategory, articlesEndpoints, homepageEndpoint } from './routes';
 import { IArticleResponse } from '@/interfaces/latest';
 
 const CACHE_REVALIDATION = 50;
 
 export const getArticles = async (slug: string): Promise<IArticle> =>
   await fetch(articleEndpoint(slug), { next: { revalidate: CACHE_REVALIDATION } })
+    .then(res => res.json());
+
+export const getArticleById = async (id: string): Promise<IArticle> =>
+  await fetch(articleEndpointById(id), { cache: 'no-cache' })
     .then(res => res.json());
 
 
@@ -23,3 +27,5 @@ export const getLatestArticles = async (): Promise<IArticleResponse> =>
 export const getRelatedArticles = async (category: string): Promise<IArticleResponse> =>
   await fetch(articlesByCategory(category), { next: { revalidate: CACHE_REVALIDATION } })
     .then(res => res.json());
+
+
